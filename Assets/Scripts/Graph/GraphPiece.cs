97c309267e13329d48fdc12857342600a68699ca
Simone Guggiari <simogecko@gmail.com>
2019-01-26 14:06:21 +0100

    built map

--- Assets/Scripts/Graph/GraphPiece.cs
+++ Assets/Scripts/Graph/GraphPiece.cs
@@ -8,9 +8,11 @@ public class GraphPiece : MonoBehaviour
     public string xJunction = "1,2, 1,4, 1,6, 1,0, 3,4, 3,6, 3,0, 3,2, 5,6, 5,0, 5,2, 5,4, 7,0, 7,2, 7,4, 7,6"; // X junction
     public string iJunction = "1,2, 1,0, 3,0, 3,2";
 
-    public Segment[] segments;
+    public string junction;
+
 
     public Transform[] vertices;
+    public Segment[] segments;
     public EdgeSegment[] edges;
 
     // Start is called before the first frame update
@@ -27,15 +29,22 @@ public class GraphPiece : MonoBehaviour
 
     [ContextMenu("generate edges")]
     void GenerateEdges() {
+        //find vertices
+
+        //find segments
+
+
+        //generate edges
         List<EdgeSegment> res = new List<EdgeSegment>();
-        string[] pieces = xJunction.RemoveSpaceAndTabs().Split(',');
+        string[] pieces = junction.RemoveSpaceAndTabs().Split(',');
+        Debug.Log("pc=" + pieces.Length);
+        Debug.Log("sl=" + segments.Length);
+        Debug.Assert(segments.Length == pieces.Length / 2, "segment size mismatch");
+
         for (int i = 0; i < pieces.Length; i += 2) {
             int fr = int.Parse(pieces[i]);
             int to = int.Parse(pieces[i + 1]);
-            if(segments!=null && i/2<segments.Length)
-                res.Add(new EdgeSegment(fr, to, segments[i / 2]));
-            else
-                res.Add(new EdgeSegment(fr, to, null));
+            res.Add(new EdgeSegment(fr, to, segments[i / 2]));
         }
         edges = res.ToArray();
     }
@@ -50,8 +59,9 @@ public class GraphPiece : MonoBehaviour
         }
         Gizmos.color = Color.grey;
         foreach (EdgeSegment e in edges) {
-            if(e!=null)
-                Gizmos.DrawLine(vertices[e.from].position, vertices[e.to].position);
+            if (e != null) {
+                //Gizmos.DrawLine(vertices[e.from].position, vertices[e.to].position);
+            }
         }
     }
 
